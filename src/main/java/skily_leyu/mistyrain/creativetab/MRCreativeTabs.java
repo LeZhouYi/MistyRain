9f6744ecfef33645a239ca873263554c1539227f
mhhu <mhhu@liangzhicn.com>
2021-07-28 19:50:44 +0800

    add some describe

--- src/main/java/skily_leyu/mistyrain/creativetab/MRCreativeTabs.java
+++ src/main/java/skily_leyu/mistyrain/creativetab/MRCreativeTabs.java
@@ -4,6 +4,11 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
+/**
+ * MR相关创造物品栏
+ * @author Skily
+ * @version 1.0.0
+ */
 public class MRCreativeTabs {
     public static final CreativeTabs tabMistyRain = new CreativeTabs("mistyRain") {
 
