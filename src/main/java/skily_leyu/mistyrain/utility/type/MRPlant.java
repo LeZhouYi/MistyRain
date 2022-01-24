980b0c48f3e79ee449a80585eb82ee61308908dd
LeZhouYi <1909944159@qq.com>
2022-01-25 05:37:20 +0800

    update

--- src/main/java/skily_leyu/mistyrain/utility/type/MRPlant.java
+++ src/main/java/skily_leyu/mistyrain/utility/type/MRPlant.java
@@ -2,6 +2,7 @@ package skily_leyu.mistyrain.utility.type;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import net.minecraft.util.EnumFacing;
 
@@ -21,6 +22,8 @@ public class MRPlant {
     protected Map<PlantStage,List<String>> productItems; //对应生长阶段的产物
     protected Map<PlantStage,MRAnimal> productAnimal; //灵气产物
     protected List<EnumFacing> facings; //灵气逸散方向
+    protected List<PlantPlan> plans; //植物的生长计划
+
 
     /**
      * 植物生长状态
@@ -42,7 +45,42 @@ public class MRPlant {
     public static class PlantPlan{
         private PlantStage nowStage; //目前状态
         private List<PlantStage> toStage; //对应状态
-        private List<Integer> weight; //转换权重
+        private List<Integer> weights; //转换权重,以1000为分母，weights[0]为第一个权重，weigts[1]-weigths为第二个权重
+        private List<MRSolarTerm> term; //0=开始时间，1=结束时间
+
+        public PlantStage getNowPlantStage(){
+            return this.nowStage;
+        }
+
+        /**
+         * 判断是否是合适的生长期
+         * @param term
+         * @return
+         */
+        public boolean isSuitTerm(MRSolarTerm term){
+            int start = this.term.get(0).ordinal();
+            int end = this.term.get(1).ordinal();
+            int now = term.ordinal();
+            return (start<=now&&now<=end)||(now>=start&&start>end);
+        }
+
+        /**
+         * 返回当前植物状态要转换的状态
+         * @param rand
+         * @return
+         */
+        public PlantStage getToStage(Random rand){
+            if(toStage.size()>1){
+                int randValue = rand.nextInt(1000);
+                for(int i = 0;i<toStage.size();i++){
+                    if(weights.get(i)>randValue){
+                        return toStage.get(i);
+                    }
+                }
+            }
+            return toStage.get(0);
+        }
+
     }
 
 }
--- src/main/java/skily_leyu/mistyrain/utility/type/MRSolarTerm.java
+++ src/main/java/skily_leyu/mistyrain/utility/type/MRSolarTerm.java
@@ -5,37 +5,43 @@ package skily_leyu.mistyrain.utility.type;
  */
 public enum MRSolarTerm {
 
-    SPRING_BEGINS(2.1F,2.6F),
-    THE_RAINS(2.6F,3.17F),
-    INSECTS_AWAKEN(3.17F,3.67F),
-    VERNAL_EQUINOX(3.67F,4.17F),
-    CLEAR_AND_BRIGHT(4.17F,4.63F),
-    GRAIN_RAIN(4.63F,5.17F),
-    SUMMER_BEGINS(5.17F,5.67F),
-    GRAIN_BUDS(5.67F,6.17F),
-    GRAIN_IN_EAR(6.17F,6.7F),
-    SUMMER_SOLSTICE(6.7F,7.23F),
-    SLIGHT_HEAT(7.23F,7.73F),
-    GREAT_HEAT(7.73F,8.2F),
-    AUTUMN_BEGINS(8.2F,8.73F),
-    STOPPING_THE_HEAT(8.73F,9.23F),
-    WHITE_DEWS(9.23F,9.73F),
-    AUTUMN_EQUINOX(9.73F,10.23F),
-    COLD_DEWS(10.23F,10.77F),
-    HOAR_FROST_FALLS(10.77F,11.23F),
-    WINTER_BEGINS(11.23F,11.73F),
-    LIGHT_SNOW(11.73F,12.23F),
-    HEAVY_SNOW(12.23F,12.73F),
-    WINTER_SOLSTICE(12.73F,1.17F),
-    SLIGHT_COLD(1.17F,1.63F),
-    GREAT_COLD(1.63F,2.1F);
+    SPRING_BEGINS(2.1F,2.6F,Season.SPRING),
+    THE_RAINS(2.6F,3.17F,Season.SPRING),
+    INSECTS_AWAKEN(3.17F,3.67F,Season.SPRING),
+    VERNAL_EQUINOX(3.67F,4.17F,Season.SPRING),
+    CLEAR_AND_BRIGHT(4.17F,4.63F,Season.SPRING),
+    GRAIN_RAIN(4.63F,5.17F,Season.SPRING),
+    SUMMER_BEGINS(5.17F,5.67F,Season.SUMMER),
+    GRAIN_BUDS(5.67F,6.17F,Season.SUMMER),
+    GRAIN_IN_EAR(6.17F,6.7F,Season.SUMMER),
+    SUMMER_SOLSTICE(6.7F,7.23F,Season.SUMMER),
+    SLIGHT_HEAT(7.23F,7.73F,Season.SUMMER),
+    GREAT_HEAT(7.73F,8.2F,Season.SUMMER),
+    AUTUMN_BEGINS(8.2F,8.73F,Season.AUTUMN),
+    STOPPING_THE_HEAT(8.73F,9.23F,Season.AUTUMN),
+    WHITE_DEWS(9.23F,9.73F,Season.AUTUMN),
+    AUTUMN_EQUINOX(9.73F,10.23F,Season.AUTUMN),
+    COLD_DEWS(10.23F,10.77F,Season.AUTUMN),
+    HOAR_FROST_FALLS(10.77F,11.23F,Season.AUTUMN),
+    WINTER_BEGINS(11.23F,11.73F,Season.WINTER),
+    LIGHT_SNOW(11.73F,12.23F,Season.WINTER),
+    HEAVY_SNOW(12.23F,12.73F,Season.WINTER),
+    WINTER_SOLSTICE(12.73F,1.17F,Season.WINTER),
+    SLIGHT_COLD(1.17F,1.63F,Season.WINTER),
+    GREAT_COLD(1.63F,2.1F,Season.WINTER);
 
     private float start;
     private float end;
+    private Season season;
 
-    private MRSolarTerm(float start, float end){
+    private MRSolarTerm(float start, float end, Season season){
         this.start = start;
         this.end = end;
+        this.season = season;
+    }
+
+    public Season getSeason(){
+        return this.season;
     }
 
     /**
@@ -51,6 +57,13 @@ public enum MRSolarTerm {
             }
         }
         return MRSolarTerm.WINTER_SOLSTICE; //均不符合时只有此节气符合
+    }
+
+    public static enum Season{
+        SPRING,
+        SUMMER,
+        AUTUMN,
+        WINTER;
     }
 
 }
